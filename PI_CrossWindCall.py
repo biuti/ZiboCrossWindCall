62b92d24bc130c515b59f1636080afeb2ecbb801
Antonio Golfari <antoniogolfari@gmail.com>
2026-01-15 10:27:42 +0100

    Add initial documentation and license information

---
+++ PI_CrossWindCall.py
@@ -0,0 +1,10 @@
+"""
+CrossWindCall
+X-Plane plugin
+
+Copyright (c) 2026, Antonio Golfari
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. 
+"""
